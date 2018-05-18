8e27be011095ded0b29603f0a29b753614600a7c
Olli Ahonen <olli>
2018-05-18 15:42:26 +0300

    3.2

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -52,7 +52,10 @@ object List { // `List` companion object. Contains functions for creating and wo
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
 
-  def tail[A](l: List[A]): List[A] = ???
+  def tail[A](l: List[A]): List[A] = l match {
+    case Cons(_, t) => t
+    case _ => Nil
+  }
 
   def setHead[A](l: List[A], h: A): List[A] = ???
 
